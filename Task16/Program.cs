feda3ece4af0630ec2aecfeccd086e34b23c3f40
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-11-19 03:18:29 +0300

    Добавьте файлы проекта.

---
+++ Task16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int x = Gcd.CalculateGcd(Gcd.BinaryAlgorithm, 4, 16, 16, 8);
+            sw.Stop();
+            Console.WriteLine("x = "+x);
+            Console.WriteLine(sw.Elapsed);
+
+            sw.Reset();
+
+            sw.Start();
+            int y = Gcd.CalculateGcd(Gcd.EuclideanAlgorithm, 4, 16, 16, 8);
+            sw.Stop();
+            Console.WriteLine("y = "+y);
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
